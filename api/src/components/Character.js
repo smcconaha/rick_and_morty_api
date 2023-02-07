b5b4b4955eb5584639e95355f81e0719e0f0fee1
smcconaha <steve.mcconaha@student.awesomeinc.org>
2023-02-07 18:37:37 +0000

    Added map to Character component that displays each item from the array.

--- api/src/components/Character.js
+++ api/src/components/Character.js
@@ -1,28 +1,36 @@
 import React from "react";
 
 function Character(props) {
-    console.log(props.charData.results.map(result => result.name));
-    let char = props.charData.results;
-    console.log(char)
-    let charName;
-    return (
-        <div className='col-3'>
+    // console.log(props.charData.results.map(result => result.name));
+    let charData = props.charData.results.map(result => 
+        (
+            <div className='col-3'>
             <div className="char-card mb-3">
                 <div className="row g-0">
                     <div className="col-md-4">
-                    <img src="#" className="img-fluid rounded-start" alt="#"/>
+                    <img src={result.image} className="img-fluid rounded-start" alt="#"/>
                     </div>
                     <div className="col-md-8">
                     <div className="card-body">
-                        <h5 className="card-title">{charName}</h5>
-                        <p className="card-text">This is a wider card with supporting text below as a natural 
-                            lead-in to additional content. This content is a little bit longer.
-                        </p>
+                        <h5 className="card-title">{result.name}</h5>
+                        <p className="card-text">Status: {result.status}</p>
+                        <p className="card-text">Species: {result.species}</p>
+                        <p className="card-text">Gender: {result.gender}</p>
+                        <p className="card-text">Origin: {result.origin.name}</p>
+                        <p className="card-text">Last known location: {result.location.name}</p>
+                        <ul>
+                            <li></li>
+                        </ul>
                     </div>
                     </div>
                 </div>
             </div>
         </div>
+        )
+    );
+
+    return (
+        <>{charData}</>
     )
 }
 export default Character;
