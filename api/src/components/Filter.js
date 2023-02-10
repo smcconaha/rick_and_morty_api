7f150135c65183ceca138f8f66d4d5910927d35f
smcconaha <steve.mcconaha@student.awesomeinc.org>
2023-02-10 18:44:06 +0000

    Added counter usehook to increment and decrememt page

--- api/src/components/Filter.js
+++ api/src/components/Filter.js
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useState , useEffect} from "react";
 
 function Filter(props) {
     // //this returns the number of pages available
     // console.log(props.charData.info.pages)
+    const [count, setCount] = useState(1);
 
     return (
         <>
             <div className="row justify-content-center">
                 <div className = "col text-center">
-                    <p className="page-text"><small className="text-muted">Page # of {props.charData.info.pages}</small></p>
+                    <p className="page-text"><small className="text-muted">Page {count} of {props.charData.info.pages}</small></p>
                 </div>
             </div>        
             <div className="row">
@@ -16,10 +17,10 @@ function Filter(props) {
                     <nav aria-label="Page navigation example">
                         <ul className="pagination justify-content-center">
                             <li className="page-item">
-                                <a className="page-link">Previous</a>
+                                <a className="page-link" onClick={() => setCount(count -1)}>Previous</a>
                             </li>
                             <li class="page-item">
-                                <a className="page-link">Next</a>
+                                <a className="page-link" onClick={() => setCount(count + 1)}>Next</a>
                             </li>
                         </ul>
                     </nav>
